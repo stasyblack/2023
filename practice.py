4a20e1749475df9eca0851a6d07fcf1a4b486271
stasyblack <73378404+stasyblack@users.noreply.github.com>
2023-07-13 16:38:56 +0300

    Add files via upload

--- practice.py
+++ practice.py
@@ -4,15 +4,19 @@ def split_image(image_path, tile_size):
     dataset = gdal.Open(image_path)
     width = dataset.RasterXSize
     height = dataset.RasterYSize
-    k=0
+
     for i in range(0, width, tile_size):
         for j in range(0, height, tile_size):
+            if (i + tile_size <= width) and (j + tile_size <= height):
                 tile = dataset.ReadAsArray(i, j, tile_size, tile_size)
-                #вызов функции по обработке
+                process_tile(tile)
 
+def process_tile(tile):
+    print('Обработка изображения')
 
+def main():
+    image_path = "NHT.tif"
+    tile_size = 512
+    split_image(image_path, tile_size)
 
-
-image_path = "NHT.tif"
-tile_size = 512
-split_image(image_path, tile_size)
+main()
