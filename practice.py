0246d3f67a476bd8c4916adfbd88b21dc7e619f2
stasyblack <73378404+stasyblack@users.noreply.github.com>
2023-07-17 14:27:54 +0300

    С наложением

--- practice.py
+++ practice.py
@@ -1,22 +1,38 @@
 from osgeo import gdal
+import numpy as np
 
-def split_image(image_path, tile_size):
+def split_image(image_path, tile_size, overlap):
     dataset = gdal.Open(image_path)
     width = dataset.RasterXSize
     height = dataset.RasterYSize
+    num_blocks_x = int(np.ceil((width - overlap) / (tile_size - overlap)))
+    num_blocks_y = int(np.ceil((height - overlap) / (tile_size - overlap)))
+    tile = np.zeros((num_blocks_y, num_blocks_x, tile_size, tile_size, dataset.RasterCount), dtype=np.uint8)
 
-    for i in range(0, width, tile_size):
-        for j in range(0, height, tile_size):
-            if (i + tile_size <= width) and (j + tile_size <= height):
-                tile = dataset.ReadAsArray(i, j, tile_size, tile_size)
-                process_tile(tile)
+    for i in range(num_blocks_y):
+        for j in range(num_blocks_x):
+            y_start = i * (tile_size - overlap)
+            x_start = j * (tile_size - overlap)
+            y_end = y_start + tile_size
+            x_end = x_start + tile_size
+            if y_end > height:
+                y_end = height
+                y_start = y_end - tile_size
+            if x_end > width:
+                x_end = width
+                x_start = x_end - tile_size
+            for k in range(dataset.RasterCount):
+                band = dataset.GetRasterBand(k+1)
+                tile[i, j, :, :, k] = band.ReadAsArray(x_start, y_start, tile_size, tile_size)
+    process_tile(tile)
 
 def process_tile(tile):
-    print('Обработка изображения')
+    print('Обработка массива изображений')
 
 def main():
     image_path = "NHT.tif"
     tile_size = 512
-    split_image(image_path, tile_size)
+    overlap = 30
+    split_image(image_path, tile_size, overlap)
 
 main()
